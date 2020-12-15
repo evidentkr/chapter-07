5345bd1cac6cd847a7f328a862fa9231c7de7afd
김성민 <evidentkr@naver.com>
2020-12-16 00:30:29 +0900

    챕터7 완료

--- src/main/java/com/rubypaper/config/SecurityConfig.java
+++ src/main/java/com/rubypaper/config/SecurityConfig.java
@@ -1,9 +1,7 @@
 package com.rubypaper.config;
 
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.context.annotation.Bean;
 //import org.springframework.beans.factory.annotation.Autowired;
 //import org.springframework.context.annotation.Bean;
 //import org.springframework.security.crypto.factory.PasswordEncoderFactories;
@@ -11,11 +9,13 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity  //시큐리티 설정 파일임을 의미
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-//	@Autowired
-//	private BoardUserDetailsService boardUserDetailsService;
+	@Autowired
+	private BoardUserDetailsService boardUserDetailsService;
 
 	@Override
 	protected void configure(HttpSecurity security) throws Exception {
@@ -29,15 +29,15 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		security.formLogin().loginPage("/login").defaultSuccessUrl("/loginSuccess", true); //사용자가 만든 로그인 보임
 		security.exceptionHandling().accessDeniedPage("/accessDenied");  //접근 권한 없는 페이지로 이동할 경우 이동
 		security.logout().invalidateHttpSession(true).logoutSuccessUrl("/login"); //로그아웃시 이동하는 주소
-//
-//		security.userDetailsService(boardUserDetailsService);
+
+		security.userDetailsService(boardUserDetailsService);
 
 	}
 	
-//	@Bean 
-//	public PasswordEncoder passwordEncoder() {
-//		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-//	}
+	@Bean 
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
 	
 	//메모리 임시 로그인 -  콘솔에 찍힌 패스워드 말고 로그인 가능
 //	@Autowired
@@ -53,7 +53,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //		.roles("ADMIN");
 //	}
 	
-	//437 데이터베이스를 이용한 로그인
+	//437 데이터베이스를 이용한 로그인 ================================================================
 //	DROP TABLE MEMBER;
 //	CREATE TABLE MEMBER(
 //		ID VARCHAR2(10) PRIMARY KEY,
@@ -66,19 +66,18 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //	INSERT INTO MEMBER VALUES('manager','manager123','매니저','ROLE_MANAGER',TRUE);
 //	INSERT INTO MEMBER VALUES('admin','admin123','어드민','ROLE_ADMIN',TRUE);
 //	COMMIT;
-	@Autowired
-	private DataSource dataSource;
 	
-	@Autowired
-	public void authenticate(AuthenticationManagerBuilder auth) throws Exception{
-		String query1 = "select id username, concat('{noop}', password) password, true enabled from member where id=?";
-		String query2 = "select id, role from member where id=?";
-		
-		auth.jdbcAuthentication()
-		.dataSource(dataSource)
-		.usersByUsernameQuery(query1)
-		.authoritiesByUsernameQuery(query2);
-		
-	}
+//	@Autowired
+//	private DataSource dataSource;
+//	
+//	@Autowired
+//	public void authenticate(AuthenticationManagerBuilder auth) throws Exception{
+//		String query1 = "select id username, concat('{noop}', password) password, true enabled from member where id=?";
+//		String query2 = "select id, role from member where id=?";
+//		auth.jdbcAuthentication()
+//		.dataSource(dataSource)
+//		.usersByUsernameQuery(query1)
+//		.authoritiesByUsernameQuery(query2);
+//	}
 
 }
